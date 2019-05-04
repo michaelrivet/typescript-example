74587caa653b1f0b5be51c9757088735ccf45665
Michael Rivet <michaelrivet9.81@gmail.com>
2019-05-03 21:38:01 -0500

    Adding enzyme and some react jest tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('the App component', () => {
+  it('renders without crashing', () => {
+    const mount = shallow(<App />);
+    expect(mount).not.toBeNull();
+  });
+  
+  it('renders a header', () => {
+    const mount = shallow(<App />);
+    expect(mount.find('div.header')).not.toBeNull();
+  });
+  
+  it('renders three containers', () => {
+    const mount = shallow(<App />);
+    expect(mount.find('div.container')).toHaveLength(3);
+  });
+})
